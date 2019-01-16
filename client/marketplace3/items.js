30f94306d9b0f62c4523bd6091307a300571ee41
josie-garza <31973143+josie-garza@users.noreply.github.com>
2019-01-16 01:46:22 -0500

    Add files via upload

---
+++ client/marketplace3/items.js
@@ -0,0 +1,74 @@
+module.exports = [
+  {
+    id: 0,
+    picture: "/static/toothpaste.jpeg",
+    name: "Toothpaste",
+    quantity: 0
+  },
+  {
+    id: 1,
+    picture: "/static/oatmeal.jpeg",
+    name: "Oatmeal",
+    quantity: 0
+  },
+  {
+    id: 2,
+    picture: "/static/pencilset.jpeg",
+    name: "Pencilset",
+    quantity: 0
+  },
+  {
+    id: 3,
+    picture: "/static/banana.jpeg",
+    name: "Bananas",
+    quantity: 0
+  },
+  {
+    id: 4,
+    picture: "/static/black-beans.jpeg",
+    name: "Black Beans (3 pack)",
+    quantity: 0
+  },
+  {
+    id: 5,
+    picture: "/static/garbanzo.jpeg",
+    name: "Garbanzo Beans (3 pack)",
+    quantity: 0
+  },
+  {
+    id: 6,
+    picture: "/static/shampoo.jpeg",
+    name: "Shampoo",
+    quantity: 0
+  },
+  {
+    id: 7,
+    picture: "/static/conditioner.jpeg",
+    name: "Conditioner",
+    quantity: 0
+  },
+  {
+    id: 8,
+    picture: "/static/bodywash.png",
+    name: "Body Wash",
+    quantity: 0
+  },
+  {
+    id: 9,
+    picture: "/static/facewash.jpeg",
+    name: "Face Wash",
+    quantity: 0
+  },
+  {
+    id: 10,
+    picture: "/static/women-vitamin.jpeg",
+    name: "Women's Vitamins",
+    quantity: 0
+  },
+  {
+    id: 11,
+    picture: "/static/toiletpaper.jpeg",
+    name: "Toilet Paper",
+    quantity: 0
+  }
+]
